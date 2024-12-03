4d6bc8c5f7abc0a812b156af5bbbd0a2550b3fbf
benjamin.kogan <benjamin.kogan@bytedance.com>
2024-12-03 00:23:22 -0500

    3 simplified

--- 3.py
+++ 3.py
@@ -1,31 +1,25 @@
 import re
-from aoc import read_input
+from aoc import nums, read_input
 
-r = re.compile("mul\\((\\d+),(\\d+)\\)")
-do = re.compile("do\\(\\)")
-dont = re.compile("don't\\(\\)")
+r1 = re.compile("mul\\(\\d+,\\d+\\)")
+r2 = re.compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)")
 
 lines = read_input(split_lines=False)
 total = 0
-for m in r.finditer(lines):
-    a, b = m.groups()
-    total += int(a) * int(b)
+for m in r1.findall(lines):
+    ns = nums(m)
+    total += ns[0] * ns[1]
 print(total)
-
-matches = [(m.groups(), m.span()[0]) for m in r.finditer(lines)]
-dos = [("do", m.span()[0]) for m in do.finditer(lines)]
-donts = [("dont", m.span()[0]) for m in dont.finditer(lines)]
-a = matches + dos + donts
-a = sorted(a, key=lambda x: x[1])
 
 enabled = True
 total = 0
-for k, _ in a:
-    if k == "do":
+for m in r2.findall(lines):
+    if m == "do()":
         enabled = True
-    elif k == "dont":
+    elif m == "don't()":
         enabled = False
     elif enabled:
-        total += int(k[0]) * int(k[1])
+        ns = nums(m)
+        total += ns[0] * ns[1]
 
 print(total)
