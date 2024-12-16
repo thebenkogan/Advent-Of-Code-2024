9ab8f68bdd0ba57c47264c22df877a1133cfd829
Ben Kogan <benkogan9@gmail.com>
2024-12-15 23:07:55 -0800

    16 optimized

--- 16.py
+++ 16.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import heapq
+import math
 from aoc import DIRS, read_input
 
 lines = read_input()
@@ -6,7 +8,6 @@ grid = [[c for c in line] for line in lines]
 C = len(grid[0])
 R = len(grid)
 
-dx, dy = 1, 0
 for y, line in enumerate(grid):
     for x, c in enumerate(line):
         if c == "S":
@@ -14,52 +15,61 @@ for y, line in enumerate(grid):
         if c == "E":
             ex, ey = x, y
 
+q = [(0, sx, sy, 1, 0)]
+costs = defaultdict(lambda: math.inf)
+costs[(sx, sy, 1, 0)] = 0
+froms = defaultdict(set)
+seen = set()
+while len(q) > 0:
+    d, x, y, dx, dy = heapq.heappop(q)
+    if (x, y, dx, dy) in seen:
+        continue
+    seen.add((x, y, dx, dy))
 
-# returns the cost and positions of the shortest path from sx, sy to ex, ey
-def dijkstra(grid, sx, sy, ex, ey):
-    q = [(0, sx, sy, 1, 0, {(sx, sy)})]
-    seen = set()
-    while len(q) > 0:
-        d, x, y, dx, dy, path = heapq.heappop(q)
-        if (x, y) in seen:
-            continue
-        seen.add((x, y))
-
-        if (x, y) == (ex, ey):
-            return d, path
-
-        for ddx, ddy in DIRS:
-            if ddx == -dx and ddy == -dy:
-                continue
-            nx, ny = x + ddx, y + ddy
-            is_straight = abs(ddx) == abs(dx) and abs(ddy) == abs(dy)
-            cost = 1
-            if not is_straight:
-                cost += 1000
-
-            if grid[ny][nx] == "#" or (nx, ny) in seen:
-                continue
-
-            new_path = path.copy()
-            new_path.add((nx, ny))
-            heapq.heappush(q, (d + cost, nx, ny, ddx, ddy, new_path))
-
-    return None, None
-
-
-lowest, path = dijkstra(grid, sx, sy, ex, ey)
-spots = set()
-spots.update(path)
-
-# for each position along the path, replace it with a wall and try dijkstra again
-for px, py in path:
-    if (px, py) == (ex, ey) or (px, py) == (sx, sy):
+    if (x, y) == (ex, ey):
         continue
 
-    grid[py][px] = "#"
-    cost, p = dijkstra(grid, sx, sy, ex, ey)
-    if cost == lowest:
-        spots.update(p)
-    grid[py][px] = "."
+    for ddx, ddy in DIRS:
+        if ddx == -dx and ddy == -dy:
+            continue
+        nx, ny = x + ddx, y + ddy
+        is_straight = abs(ddx) == abs(dx) and abs(ddy) == abs(dy)
+        cost = 1
+        if not is_straight:
+            cost = 1000
+            nx, ny = x, y
 
-print(len(spots))
+        new_cost = d + cost
+        nxt = (nx, ny, ddx, ddy)
+        curr = (x, y, dx, dy)
+        if grid[ny][nx] == "#" or nxt in seen:
+            continue
+
+        if new_cost > costs[nxt]:
+            continue
+        elif new_cost == costs[nxt]:
+            froms[nxt].add(curr)
+            continue
+
+        costs[nxt] = new_cost
+        froms[nxt] = {curr}
+        heapq.heappush(q, (new_cost, nx, ny, ddx, ddy))
+
+best_dir = None
+best_cost = math.inf
+for (x, y, dx, dy), c in costs.items():
+    if c < best_cost and (x, y) == (ex, ey):
+        best_cost = c
+        best_dir = (dx, dy)
+
+best_paths = {(ex, ey)}
+stack = [(ex, ey, best_dir[0], best_dir[1])]
+while len(stack) > 0:
+    cx, cy, dx, dy = stack.pop()
+    f = froms[(cx, cy, dx, dy)]
+    best_paths.update([(px, py) for px, py, _, _ in f])
+    stack.extend(f)
+
+
+print(best_cost)
+print(len(best_paths))
