51f1979ab0a9dce3b5c82cd4c4fe2c98eb75f183
Ben Kogan <benkogan9@gmail.com>
2024-12-21 21:42:47 -0800

    22 simplified

--- 22.py
+++ 22.py
@@ -1,47 +1,34 @@
+from collections import defaultdict, deque
 from aoc import read_input
 
 lines = read_input()
 
-total = 0
-all_seqs = set()
-rs = []
-rs_seqs = []
+p1 = 0
+totals = defaultdict(int)
 for line in lines:
     n = int(line)
-    results = []
+    window = deque()
+    seen = set()
     for _ in range(2000):
         start = n % 10
         n = (n * 64) ^ n
         n = n % 16777216
-
         n = (n // 32) ^ n
         n = n % 16777216
-
         n = (n * 2048) ^ n
         n = n % 16777216
-        results.append((n, (n % 10) - start))
+        window.append((n % 10, (n % 10) - start))
+        if len(window) > 4:
+            window.popleft()
 
-    total += n
-
-    seqs = {}
-    for i in range(0, len(results) - 3):
-        seq = tuple([results[i + j][1] for j in range(4)])
-        if seq not in seqs:
-            seqs[seq] = results[i + 3][0] % 10
-
-    all_seqs.update(seqs.keys())
-    rs.append(results)
-    rs_seqs.append(seqs)
-
-print(total)
-
-best = 0
-for seq in all_seqs:
-    total = 0
-    for r, rs_seq in zip(rs, rs_seqs):
-        if seq not in rs_seq:
+        seq = tuple([w[1] for w in window])
+        if seq in seen:
             continue
-        total += rs_seq[seq]
-    best = max(best, total)
+        seen.add(seq)
+        if len(seq) == 4:
+            totals[seq] += window[-1][0]
 
-print(best)
+    p1 += n
+
+print(p1)
+print(max(totals.values()))
